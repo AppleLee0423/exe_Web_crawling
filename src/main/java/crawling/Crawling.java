341ec28f1f95f94b7440c870612fe15d02d99beb
AppleLee0423 <ld210@naver.com>
2023-04-24 21:15:01 +0900

    Career, Pay information get ver.JobKorea

--- src/main/java/crawling/Crawling.java
+++ src/main/java/crawling/Crawling.java
@@ -17,15 +17,19 @@ public class Crawling {
 			Document doc = Jsoup.connect(URL).get();
 			// URL의 페이지 소스 불러오기
 			
-			Elements elem = doc.select(".sumTit .hd_3");
+			Elements title_elem = doc.select(".sumTit .hd_3");
 			// 불러올 태그를 Element 요소로 변환
 			
-			int num = elem.text().indexOf("닫기");
-			String title = elem.text().substring(num+3);
+			int num = title_elem.text().indexOf("닫기");
+			String title = title_elem.text().substring(num+3);
 			// title에 네이밍이 되어있지 않아, title 앞에서 substring으로 끊어줌
-			System.out.println(title);
+			//System.out.println(title);
 			
-			
+			Elements info_elem = doc.select(".artReadJobSum > .tbRow > .tbCol");
+			Elements career = info_elem.get(0).select(".tbList dd .col_1");
+			Element pay = info_elem.get(1).select(".tbList dd").get(1);
+			// 공고의 헤더에서 신입과 경력 구분, 급여 조건에 대해서 추출
+			System.out.println(pay.text());
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
